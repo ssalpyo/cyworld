edd061c9edfacd9d48ccf4de71575abc21cd403c
ssalpyo <tjdvy6@gmail.com>
2022-04-22 17:25:40 +0900

    # Fix: index.js: contentFrame

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,23 @@
 function goHome() {
+	document.getElementById("contentFrame").setAttribute("src", "home.html");
+
 	document.getElementById("btn__home").style = "background-color: #FFF; color: #000;"
 	document.getElementById("btn__game").style = "background-color: #298EB5; color: #FFF;"
 	document.getElementById("btn__jukebox").style = "background-color: #298EB5; color: #FFF;"
-	
-	document.getElementById("page__home").style = null;
-	document.getElementById("page__game").style = "visibility: hidden; display: none;";
-	document.getElementById("page__jukebox").style = "visibility: hidden; display: none;";
 }
 
 function goGame() {
+	document.getElementById("contentFrame").setAttribute("src", "game.html");
+
 	document.getElementById("btn__game").style = "background-color: #FFF; color: #000;"
 	document.getElementById("btn__home").style = "background-color: #298EB5; color: #FFF;"
 	document.getElementById("btn__jukebox").style = "background-color: #298EB5; color: #FFF;"
-
-	document.getElementById("page__game").style = null;
-	document.getElementById("page__home").style = "visibility: hidden; display: none;";
-	document.getElementById("page__jukebox").style = "visibility: hidden; display: none;";
 }
 
 function goJukebox() {
+	document.getElementById("contentFrame").setAttribute("src", "jukebox.html");
+
 	document.getElementById("btn__jukebox").style = "background-color: #FFF; color: #000;"
 	document.getElementById("btn__home").style = "background-color: #298EB5; color: #FFF;"
 	document.getElementById("btn__game").style = "background-color: #298EB5; color: #FFF;"
-
-	document.getElementById("page__jukebox").style = null;
-	document.getElementById("page__home").style = "visibility: hidden; display: none;";
-	document.getElementById("page__game").style = "visibility: hidden; display: none;";
 }
